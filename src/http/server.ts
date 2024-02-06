1342ae367126486cf84f18655138fdfe793cec42
Tamicktom <rickpokemon38@gmail.com>
2024-02-06 00:30:20 -0300

    feat - add create poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,25 @@
 //* Libraries imports
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
+const p = new PrismaClient();
 const server = fastify();
 
-server.get("/", async (request, reply) => {
-  return { hello: "world" };
+const createPollSchema = z.object({
+  title: z.string().min(1).max(128),
+});
+
+server.post("/poll", async (request, response) => {
+  const createPollBody = createPollSchema.parse(request.body);
+
+  const poll = await p.poll.create({
+    data: {
+      title: createPollBody.title,
+    },
+  });
+
+  return response.status(201).send(poll);
 });
 
 export { server };
